a0be2afb8bd838ad0a0528a5ccc289681956b209
Nitin Raj <rajnitin793@gmail.com>
2026-08-20 12:28:14 +0530

    creating a scheduler file to run injestion jobs automatically after an interval

---
+++ app/jobs/scheduler.py
@@ -0,0 +1,7 @@
+""" 
+Background Scheduler, creating scheduler to run ingestion jobs automatically 
+on an interval, so DB stayes updated withouth anyone manually running scripts
+
+wiring into main.py
+"""
+
