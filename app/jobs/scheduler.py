28c62c1295626782c87c8767c80ad53a82aa8527
Nitin Raj <rajnitin793@gmail.com>
2026-08-20 12:34:35 +0530

    done with required imports and implementing run_all_ingestion_jobs

--- app/jobs/scheduler.py
+++ app/jobs/scheduler.py
@@ -5,3 +5,11 @@ on an interval, so DB stayes updated withouth anyone manually running scripts
 wiring into main.py
 """
 
+from apscheduler.schedulers.background import BackgroundScheduler
+from app.core.config import settings
+
+from app.services.news_ingestion import ingest_all_corridors
+from app.services.eia_ingestion import ingest_eia_data
+
+scheduler = BackgroundScheduler()
+
